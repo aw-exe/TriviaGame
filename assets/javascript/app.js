419d8a15cadc91d207cbd949c8a01f5f9dcc7fbb
Ashton Wetsel <ashton.wetsel.23@gmail.com>
2019-10-21 23:24:30 -0500

    2019-10-21: Began adding code to my app.js page, including my first trivia question and the let variables for my game. To be continued.

---
+++ assets/javascript/app.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    //  trivia questions and answers
+
+    let correctAnswers = 0;
+    let incorrectAnswers = 0;
+    let unansweredQuestions = 0;
+    let timeRemaining = 15;
+    let indexQandA = 0; //index to load a different question each round without the game reset or screen refresh
+    let answered = false; //variable to stop the timer if user has clicked an answer
+    var triviaGame = [{
+        question: "What is the name of the fictional town where Stanger Things takes place?",
+        answer: ["Storybrooke", "Hawkins", "Derry", "Jasper"],
+        correct: "3",
+        image: ("<insert image here>")
+    }];
+
+   
+
+});
