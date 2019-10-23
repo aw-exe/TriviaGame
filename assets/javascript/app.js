a9425e47313363188ff92aac98549e786a13284e
Ashton Wetsel <ashton.wetsel.23@gmail.com>
2019-10-23 00:30:49 -0500

    2019-10-22: Began typing on click function to begin the trivia game.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
             let timeRemaining = 15;
             let indexQandA = 0; //index of different questions for each round without resetting the game or refreshing the page
             let answered = false; //variable to stop the timer 
-            var triviaGame = [{
+            triviaGame = [{
                 question: "What is the name of the fictional town where Stanger Things takes place?",
                 answer: ["Storybrooke", "Hawkins", "Derry", "Arnette"],
                 correct: "1",
@@ -58,4 +58,117 @@ $(document).ready(function () {
                 correct: "3",
                 image: ("<insert image here>")
 
+            }];
+
+
+            //functions
+
+
+            function startGame() {
+                console.log("game has begun");
+                $('.start-button').remove();
+                correctAnswers = 0;
+                incorrectAnswers = 0;
+                unansweredQuestions = 0;
+                loadQandA();
+            }
+        
+            // function loadQandA() {
+            //     answered = false; // will allow timeRemaining to be pushed back to <h5> after round reset....else statement in function timer()
+            //     timeRemaining = 16;
+            //     intervalID = setInterval(timer, 1000);
+            //     if (answered === false) {
+            //         timer();
+            //     }
+            //     correct = triviaGame[indexQandA].correct;
+            //     var question = triviaGame[indexQandA].question;
+            //     $('.question').html(question);
+            //     for (var i = 0; i < 4; i++) {
+            //         var answer = triviaGame[indexQandA].answer[i];
+            //         $('.answers').append('<h4 class= answersAll id=' + i + '>' + answer + '</h4>');
+            //     }
+        
+            //     $("h4").click(function () {
+            //         var id = $(this).attr('id');
+            //         if (id === correct) {
+            //             answered = true; // stops the timer
+            //             $('.question').text("THE ANSWER IS: " + triviaGame[indexQandA].answer[correct]);
+            //             correctAnswer();
+            //         } else {
+            //             answered = true; //stops the timer
+            //             $('.question').text("YOU CHOSE: " + triviaGame[indexQandA].answer[id] + ".....HOWEVER THE ANSWER IS: " + triviaGame[indexQandA].answer[correct]);
+            //             incorrectAnswer();
+            //         }
+            //     });
+            // }
+        
+            // function timer() {
+            //     if (timeRemaining === 0) {
+            //         answered = true;
+            //         clearInterval(intervalID);
+            //         $('.question').text("THE CORRECT ANSWER IS: " + triviaGame[indexQandA].answer[correct]);
+            //         unAnswered();
+            //     } else if (answered === true) {
+            //         clearInterval(intervalID);
+            //     } else {
+            //         timeRemaining--;
+            //         $('.timeRemaining').text('YOU HAVE ' + timeRemaining + ' SECONDS TO CHOOSE');
+            //     }
+            // }
+        
+            // function correctAnswer() {
+            //     correctAnswers++;
+            //     $('.timeRemaining').text("YOU HAVE ANSWERED CORRECTLY!").css({
+            //         'color': '#3D414F'
+            //     });
+            //     resetRound();
+            // }
+        
+            // function incorrectAnswer() {
+            //     incorrectAnswers++;
+            //     $('.timeRemaining').text("YOU HAVE ANSWERED INCORRECTLY!").css({
+            //         'color': '#3D414F'
+            //     });
+            //     resetRound();
+        
+            // }
+        
+            // function unAnswered() {
+            //     unansweredQuestions++;
+            //     $('.timeRemaining').text("YOU FAILED TO CHOOSE AN ANSWER").css({
+            //         'color': '#3D414F'
+            //     });
+            //     resetRound();
+            // }
+        
+            // function resetRound() {
+            //     $('.answersAll').remove();
+            //     $('.answers').append('<img class=answerImage width="150" height="150" src="' + triviaGame[indexQandA].image + ' ">'); // adds answer image
+            //     indexQandA++; // increments index which will load next question when loadQandA() is called again
+            //     if (indexQandA < triviaGame.length) {
+            //         setTimeout(function () {
+            //             loadQandA();
+            //             $('.answerImage').remove();
+            //         }, 5000); // removes answer image from previous round
+            //     } else {
+            //         setTimeout(function () {
+            //             $('.question').remove();
+            //             $('.timeRemaining').remove();
+            //             $('.answerImage').remove();
+            //             $('.answers').append('<h4 class= answersAll end>CORRECT ANSWERS: ' + correctAnswers + '</h4>');
+            //             $('.answers').append('<h4 class= answersAll end>INCORRECT ANSWERS: ' + incorrectAnswers + '</h4>');
+            //             $('.answers').append('<h4 class= answersAll end>UNANSWERED QUESTIONS: ' + unansweredQuestions + '</h4>');
+            //             setTimeout(function () {
+            //                 location.reload();
+            //             }, 7000);
+            //         }, 5000);
+            //     }
+            // };
+        
+            $('.startButton').on("click", function () {
+                $('.startButton');
+                startGame();
+        
             });
+        
+        });
